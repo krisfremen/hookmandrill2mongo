c97442be258bbf4f525cafcc36a61a2b210d3832
Kristijan "Fremen" Velkovski <me@krisfremen.com>
2015-02-02 01:36:48 -0500

    WSGIfying, returning OK when successful, exception error otherwise.

--- HookMandrill2Mongo.py
+++ HookMandrill2Mongo.py
@@ -3,20 +3,22 @@
 from flask import *
 from pymongo import MongoClient
 
-app = Flask(__name__)
+application = app = Flask(__name__)
 dbhost = "localhost"
 dbname = "webhooks"
 dbcoll = "mandrill"
-db = MongoClient(host=dbhost)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def hook():
-    mongo = MongoClient()
-    mandrillhookjson = json.loads(request.form['mandrill_events'])
-    for mandrillhook in mandrillhookjson:
-        mongo[dbname][dbcoll].insert({"hook": mandrillhook})
-    return ""
+    try:
+        mongo = MongoClient(host=dbhost)
+        mandrillhookjson = json.loads(request.form['mandrill_events'])
+        for mandrillhook in mandrillhookjson:
+            mongo[dbname][dbcoll].insert({"hook": mandrillhook})
+        return "OK"
+    except Exception, err:
+        return str(err)
 
 
 if __name__ == '__main__':
